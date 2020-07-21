b84b26e1a132cb4fa2499344be2abae686690824
Melinda Diaz <melindadiaz@me.com>
2020-07-21 22:10:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,11 +2,17 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
  
- data.each_with_object({}) do |(key, value), pigeon_list|
+ data.each_with_object({}) do |(key, value), names_array|
   value.each do |inner_key, names|
     names.each do |name|
-      binding.pry
- 
+      if !names_array[name]
+        names_array[name] = {}
+      end
+      if !names_array[name][key]
+        names_array[name][key] = []
+      end
+      names_array[name][key].push(inner_key.to_s)
+  
  end
  end
  end
