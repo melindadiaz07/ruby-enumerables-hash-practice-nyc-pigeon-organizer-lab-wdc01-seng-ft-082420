547a8051aa7f7a3febb95349005e3a2f6de0a784
Melinda Diaz <melindadiaz@me.com>
2020-07-21 22:04:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
  
- data.each_with_object({}) do |(key, value), new_hash|
+ data.each_with_object({}) do |(key, value), |
   value.each do |inner_key, names|
     names.each do |name|
       binding.pry
